6ef3b20b425354cc6ce81cc7cc0d43be3282fdac
dem4ply <dem4ply@gmail.com>
2026-07-28 05:06:21 -0600

    Bump version: 1.3.0 → 1.3.1

--- chibi_browser/__init__.py
+++ chibi_browser/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = """dem4ply"""
 __email__ = 'dem4ply@gmail.com'
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 
 from .chibi_browser import Chibi_browser
--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     name='chibi_browser',
     packages=find_packages(include=['chibi_browser', 'chibi_browser.*']),
     url='https://github.com/dem4ply/chibi_browser',
-    version='1.3.0',
+    version='1.3.1',
     zip_safe=False,
 )
